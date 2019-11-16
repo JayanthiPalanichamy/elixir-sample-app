4729e1baee08cc73587c8e12ccd4c8f7bf7abfb5
Jayanthi Palanichamy <jayanthi.p@thoughtworks.com>
2019-11-16 12:44:44 +0530

    get stock with highest last price

--- lib/highest_stock.ex
+++ lib/highest_stock.ex
@@ -1,4 +1,6 @@
 defmodule HighestStock do
+  @url "http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol="
+
   @moduledoc """
   Documentation for HighestStock.
   """
@@ -14,5 +16,26 @@ defmodule HighestStock do
   """
   def hello do
     :world
+  end
+
+  def stockFor(stock_code) do
+    request = [@url, stock_code]
+    |> Path.join("")
+    |> HTTPoison.get()
+    case request do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        {:ok,  Jason.decode!(body)}
+  end
+  end
+
+  @stock ~w(FB GOOGL MSFT AMZN)
+
+  def highest() do
+
+    stocks = Enum.map(@stock, fn (stock) -> stockFor(stock) end)
+    Enum.map(stocks, fn({:ok, body}) -> body end)
+    |> Enum.max_by(fn (body) -> body["LastPrice"] end)
+    |> Map.get("Name")
+
   end
 end
